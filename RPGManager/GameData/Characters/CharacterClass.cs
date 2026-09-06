2323c9976854bd1626e8555e2a73bdbdd5191eaf
Raxarox <agushullman111@gmail.com>
2026-09-06 12:54:31 -0300

    Add archetype support to CharacterClass with AvailableArchetypeIds and ArchetypeChoiceLevel

--- RPGManager/GameData/Characters/CharacterClass.cs
+++ RPGManager/GameData/Characters/CharacterClass.cs
@@ -8,27 +8,38 @@ public class CharacterClass
     public string ClassId { get; init; }
     public string Name { get; init; }
     public int HitDieValue { get; init; }
-    public bool IsSpellcaster { get; init; }
     public List<string> WeaponProficiencies { get; init; }
     public List<string> ArmorProficiencies { get; init; }
+    public List<ClassLevelData> Progression { get; init; }
+
+    // Which Archetype.ArchetypeId values a character of this class may choose from.
+    public List<string> AvailableArchetypeIds { get; init; }
+
+    // The level at which an archetype choice becomes available.
+    // 0 means this class currently offers no archetype choice.
+    public int ArchetypeChoiceLevel { get; init; }
 
     [JsonConstructor]
     public CharacterClass(
         string classId,
         string name,
         int hitDieValue,
-        bool isSpellcaster,
         List<string>? weaponProficiencies,
-        List<string>? armorProficiencies)
+        List<string>? armorProficiencies,
+        List<ClassLevelData>? progression,
+        List<string>? availableArchetypeIds,
+        int archetypeChoiceLevel)
     {
         ValidationHelper.ValidatePositiveInteger(name, "Class", nameof(hitDieValue), hitDieValue);
 
         ClassId = classId ?? throw new ArgumentNullException(nameof(classId));
         Name = name ?? throw new ArgumentNullException(nameof(name));
         HitDieValue = hitDieValue;
-        IsSpellcaster = isSpellcaster;
         WeaponProficiencies = weaponProficiencies ?? [];
         ArmorProficiencies = armorProficiencies ?? [];
+        Progression = progression ?? [];
+        AvailableArchetypeIds = availableArchetypeIds ?? [];
+        ArchetypeChoiceLevel = archetypeChoiceLevel;
     }
 
     public override string ToString() => Name;
