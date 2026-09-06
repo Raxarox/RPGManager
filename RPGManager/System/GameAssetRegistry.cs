a990e0354d1831d88c967ea7870b56d8322eab41
Raxarox <agushullman111@gmail.com>
2026-09-06 12:55:25 -0300

    Add Archetypes dictionary to GameAssetRegistry for archetype asset management

--- RPGManager/System/GameAssetRegistry.cs
+++ RPGManager/System/GameAssetRegistry.cs
@@ -5,8 +5,10 @@ namespace RPGManager.System;
 
 public class GameAssetRegistry(
     IReadOnlyDictionary<string, CharacterClass> classes,
-    IReadOnlyDictionary<string, Item> items)
+    IReadOnlyDictionary<string, Item> items,
+    IReadOnlyDictionary<string, Archetype> archetypes)
 {
     public IReadOnlyDictionary<string, CharacterClass> Classes { get; init; } = classes;
     public IReadOnlyDictionary<string, Item> Items { get; init; } = items;
+    public IReadOnlyDictionary<string, Archetype> Archetypes { get; init; } = archetypes;
 }
